8b7bdeac1d69ffbff2d8c3c7a858cde21f04f56d
Pieter B <pieterbeernink@hotmail.com>
2022-11-11 00:10:47 +0100

    add nuget packages and prep program.cs a bit

---
+++ EnergyDataSystem/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyDataSystem;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+    {
+        // Specific conventions can be added here
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {        
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+
+    // DbSets for each table
+   /* public DbSet<Customer> Customers { get; set; }
+    public DbSet<Owner> Owners { get; set; }    
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Building> Buildings { get; set; }
+    public DbSet<BuildingGroup> BuildingGroups { get; set; }
+    public DbSet<EnergyConnection> EnergyConnections { get; set; }
+    public DbSet<EnergyConnectionType> EnergyConnectionTypes { get; set; }*/
+}
--- EnergyDataSystem/Program.cs
+++ EnergyDataSystem/Program.cs
@@ -1,1 +1,47 @@
-﻿
+﻿using EnergyDataSystem;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// IgnoreCycles to get rid of JSON cycle error
+// https://docs.microsoft.com/en-us/dotnet/api/system.text.json.serialization.referencehandler.preserve?view=net-6.0
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions
+                .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+// Create connection string and DbContext service
+var connectionString = builder.Configuration.GetConnectionString("EDS_Db_Connection");
+
+builder.Services.AddDbContext<ApplicationDbContext>
+    (options => options.UseSqlServer(connectionString));
+
+// Inject dependencies - implementation of the repo's when called
+//
+
+
+// Trigger creation of Automapper maps specified in AutoMapperProfiles.cs
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
