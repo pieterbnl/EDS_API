cdc22dcae71931f675f1048dc81228507c7c5bdd
Pieter B <pieterbeernink@hotmail.com>
2022-11-12 13:08:10 +0100

    add some more records to seeding script

--- EnergyDataSystemAPI/Utilities/seeding_script.sql
+++ EnergyDataSystemAPI/Utilities/seeding_script.sql
@@ -78,4 +78,105 @@ INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
 	VALUES ('Gas meter 1', 'Gas meter Model X', '3', '1', '6','1','1');
 		
 INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
-	VALUES ('Gas meter 1', 'Gas meter Model X', '0', '2', '12','0','1');
+	VALUES ('Gas meter 2', 'Gas meter Model X', '0', '2', '12','0','1');
+
+
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Electicity meter 1', 'Electricity meter Model XYZ', '2', '0', '3','0','2');
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Electicity meter 2', 'Electricity meter Model XYZ', '2', '0', '3','0','2');
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Electicity meter 3', 'Electricity meter Model XYZ', '2', '0', '3','0','2');
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Water meter 1', 'Water meter Model XX1', '2', '3', '8','2','2');
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Water meter 2', 'Water meter Model XX1', '3', '3', '8','2','2');
+	
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Gas meter 1', 'Gas meter Model XX1', '3', '1', '6','1','2');
+		
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Gas meter 2', 'Gas meter Model XX1', '3', '2', '12','0','2');
+
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Electicity meter 1', 'Electricity meter Model XYZ', '2', '0', '2','0','3');
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Electicity meter 2', 'Electricity meter Model XYZ', '2', '0', '2','0','3');
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Water meter 1', 'Water meter Model XX1', '2', '3', '8','2','3');
+	
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Gas meter 1', 'Gas meter Model XX1', '3', '1', '6','1','3');
+	
+
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Electicity meter 1', 'Electricity meter Model X', '2', '0', '0','0','4');
+
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Water meter 1', 'Water meter Model X', '2', '3', '5','2','4');
+	
+INSERT INTO EnergyMeters (Name, Model, Status, Type, Capacity, Unit, BuildingId)
+	VALUES ('Gas meter 1', 'Gas meter Model X', '3', '1', '7','1','4');		
+
+
+
+----------------
+--MEASUREMENTS--
+----------------
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('1', '2022-1-12 13:02:00', '1078701');
+
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('1', '2022-1-12 13:03:00', '1078701');
+
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('1', '2022-1-12 13:04:00', '1078702');
+	
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('1', '2022-1-12 13:05:00', '1078701');  -- wrong measurement
+	
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('1', '2022-1-12 13:06:00', '1078703');
+		
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('1', '2022-1-12 13:07:00', '1078704');
+			
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('1', '2022-1-12 13:08:00', '1078705');
+
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('1', '2022-1-12 13:09:00', '1078706');
+
+
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('3', '2022-1-12 13:02:00', '678901');
+
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('3', '2022-1-12 13:03:00', '678901');
+
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('3', '2022-1-12 13:04:00', '678901');
+
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('3', '2022-1-12 13:05:00', '678902');
+	
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('3', '2022-1-12 13:06:00', '678902');
+		
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('3', '2022-1-12 13:07:00', '678902');
+			
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('3', '2022-1-12 13:08:00', '678902');
+
+INSERT INTO Measurements (EnergyMeterId, TimeStamp, Value)
+	VALUES ('3', '2022-1-12 13:09:00', '678903');
